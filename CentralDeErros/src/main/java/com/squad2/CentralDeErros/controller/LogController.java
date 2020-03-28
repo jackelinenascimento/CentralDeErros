0ccf77d8288e5598487af34f21d09ce502909df8
Jackeline do Nascimento <jackeline_nascimento@hotmail.com>
2020-03-28 15:17:49 -0300

    first commit

---
+++ CentralDeErros/src/main/java/com/squad2/CentralDeErros/controller/LogController.java
@@ -0,0 +1,90 @@
+package com.squad2.CentralDeErros.controller;
+
+import com.squad2.CentralDeErros.entity.Log;
+import com.squad2.CentralDeErros.entity.User;
+import com.squad2.CentralDeErros.entity.enumerate.Environment;
+import com.squad2.CentralDeErros.entity.enumerate.Status;
+import com.squad2.CentralDeErros.service.LogService;
+import com.squad2.CentralDeErros.service.SecurityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/log")
+//@Secured("USER")
+public class LogController {
+
+    @Autowired
+    private LogService logService;
+
+    @Autowired
+    private SecurityService securityService;
+
+    @GetMapping(params = {"id"})
+    public Optional<Log> getLogById(@RequestParam("id") Long id) {
+        return logService.getLogById(id);
+    }
+
+    @GetMapping(params = {"user"})
+    public List<Log> getLogByUserId(@RequestParam("user") Long userId,
+                                    @RequestParam(value = "status", required = false, defaultValue = "ACTIVE") Status status,
+                                    @RequestParam(value = "page", required = false, defaultValue = "0") Short page,
+                                    @RequestParam(value = "size", required = false, defaultValue = "10") Short size,
+                                    @RequestParam(value = "sortBy", required = false, defaultValue = "ID") String sortBy,
+                                    @RequestParam(value = "direction", required = false, defaultValue = "ASC") Sort.Direction direction) {
+        return logService.getLogByUserId(userId, status, page, size, sortBy, direction);
+    }
+
+    @GetMapping(params = {"user", "env"})
+    public List<Log> getLogByUserIdAndEnv(@RequestParam("user") Long userId,
+                                          @RequestParam("env") Environment environment,
+                                          @RequestParam(value = "status", required = false, defaultValue = "ACTIVE") Status status,
+                                          @RequestParam(value = "page", required = false, defaultValue = "0") Short page,
+                                          @RequestParam(value = "size", required = false, defaultValue = "10") Short size,
+                                          @RequestParam(value = "sortBy", required = false, defaultValue = "ID") String sortBy,
+                                          @RequestParam(value = "direction", required = false, defaultValue = "ASC") Sort.Direction direction) {
+        return logService.getLogByUserIdAndEnv(userId, environment, status, page, size, sortBy, direction);
+    }
+
+    @GetMapping(params = {"search"})
+    public List<Log> searchLogByEventDescription(@RequestParam("search") String keyword,
+                                                 @RequestParam(value = "status", required = false, defaultValue = "ACTIVE") Status status,
+                                                 @RequestParam(value = "page", required = false, defaultValue = "0") Short page,
+                                                 @RequestParam(value = "size", required = false, defaultValue = "10") Short size,
+                                                 @RequestParam(value = "sortBy", required = false, defaultValue = "ID") String sortBy,
+                                                 @RequestParam(value = "direction", required = false, defaultValue = "ASC") Sort.Direction direction) {
+        return logService.searchLogByEventDescriptionIgnoreCase(keyword, status, page, size, sortBy, direction);
+    }
+
+    @GetMapping(params = {"search", "env"})
+    public List<Log> searchLogByEventDescriptionAndEnvIgnoreCase(@RequestParam("search") String keyword,
+                                                                 @RequestParam("env") Environment environment,
+                                                                 @RequestParam(value = "status", required = false, defaultValue = "ACTIVE") Status status,
+                                                                 @RequestParam(value = "page", required = false, defaultValue = "0") Short page,
+                                                                 @RequestParam(value = "size", required = false, defaultValue = "10") Short size,
+                                                                 @RequestParam(value = "sortBy", required = false, defaultValue = "ID") String sortBy,
+                                                                 @RequestParam(value = "direction", required = false, defaultValue = "ASC") Sort.Direction direction) {
+        return logService.searchLogByEventDescriptionAndEnvIgnoreCase(keyword, environment, status, page, size, sortBy, direction);
+
+
+    }
+
+    @GetMapping("/teste")
+    public ResponseEntity<User> getLogTeste() {
+        try {
+            return new ResponseEntity<>(securityService.getUserAuthenticated(),HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+}
---
+++ CentralDeErros/src/main/java/com/squad2/CentralDeErros/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.squad2.CentralDeErros.repository;
+
+import com.squad2.CentralDeErros.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findById(long id);
+
+    User findByName(String name);
+
+    User findByEmail(String email);
+
+}
